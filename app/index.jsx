429a1d79030ab1af9d6a2ee9d8ae068fa03c862b
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2024-04-30 16:32:31 +0200

    configure the fonts

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-4xl">Sahre Videos!</Text>
+      <Text className="text-4xl font-pblack">Sahre Videos!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{color: 'blue'}}>Go to profile</Link>
     </View>
