4eef68bf72090979c31d09283004cb047c18a858
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2024-04-30 15:48:10 +0200

    Configure tailwind caa

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Slot, Stack } from 'expo-router'
 
-export default function App() {
+const RootLayout = () => {
   return (
-    <View style={styles.container}>
-      <Text>Sahre Videos!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+        <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+        </Stack>
+    </>
+)
 }
 
+export default RootLayout
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        display: 'flex',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
