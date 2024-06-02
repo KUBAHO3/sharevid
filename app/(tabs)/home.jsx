4b1f15e983fda3eb4fb6a0a9ae00087b8527a12a
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2024-06-02 12:25:48 +0200

    Add home header and predefined list elements with search input

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,43 @@
-import {   Text, View } from 'react-native'
+import {   FlatList, Image, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import { images } from '../../constants'
+import SearchInput from '../../components/SearchInput'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className="bg-primary">
+      <FlatList
+        data={[{id:1},{id:2},{id:3},{id:4},{id:5}]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({item}) => (
+          <Text className="text-3xl text-white">{item.id}</Text>
+        )}
+        ListHeaderComponent={() =>(
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-center flex-row mb-6">
+              <View>
+                <Text className="text-sm font-pmedium text-gray-100">
+                  Welcome back
+                </Text>
+                <Text className="text-2xl font-pmedium text-white"> Linne Heaven</Text>
+              </View>
+              <View>
+                <Image 
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode='contain'
+                />
+              </View>
+            </View>
+            <SearchInput />
+          </View>
+        )}
+      >
+
+      </FlatList>
+    </SafeAreaView>
   )
 }
 
